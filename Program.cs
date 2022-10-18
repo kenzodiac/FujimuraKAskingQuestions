a13657a1edaaf8ede0235a55d9841cd99c728551
Kenzodiac <kennethfujimura@gmail.com>
2022-10-18 06:41:10 -0700

    Initial revisions

--- Program.cs
+++ Program.cs
@@ -13,30 +13,30 @@ bool playAgain = true;
 //play again while loop
 while (playAgain == true) {
     //user inputs
-    Console.WriteLine("What is your name? Please enter on the keyboard and press enter when you are finished: ");
+    Console.Write("\nWhat is your name? Please enter on the keyboard and press enter when you are finished: ");
     string userName = Console.ReadLine();
 
-    Console.WriteLine("Very good. Now, please tell me what time you woke up today? ");
+    Console.Write("\nVery good. Now, please tell me what time you woke up today? ");
     string wakeUpTime = Console.ReadLine();
 
     //program output
-    Console.WriteLine("So you're telling me that your name is \"" + userName + "\", and that you woke up at \"" + wakeUpTime + "\"? Cool beans!");
+    Console.WriteLine("\nSo you're telling me that your name is \"" + userName + "\", and that you woke up at \"" + wakeUpTime + "\"? Cool beans!");
 
     //play again prompt with input validation
-    Console.WriteLine("Would you like to answer my questions again? Y/N");
+    Console.Write("\nWould you like to answer my questions again? Y/N: ");
     bool answerCheck = true;
     while (answerCheck == true) {
-        string playAgainInput = Console.ReadLine();
-        if (playAgainInput == "y" || playAgainInput == "Y") {
+        string playAgainInput = Console.ReadLine().ToLower();
+        if (playAgainInput == "y" || playAgainInput == "yes") {
             answerCheck = false;
             continue;
-        } else if (playAgainInput == "n" || playAgainInput == "N"){
+        } else if (playAgainInput == "n" || playAgainInput == "no"){
             playAgain = false;
             answerCheck = false;
         } else {
-            Console.WriteLine("I do not understand. Please answer with 'Y' for yes, or 'N' for no.");
+            Console.Write("\nI do not understand. Please answer with 'Y' for yes, or 'N' for no: ");
             answerCheck = true;
         }
     }
 }
-Console.WriteLine("Arigato!");
+Console.WriteLine("\nArigato!");
