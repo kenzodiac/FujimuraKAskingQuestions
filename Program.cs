3549839e3e6d60fe1ede91f3c73d6558aa920f54
Kenzodiac <kennethfujimura@gmail.com>
2022-10-18 06:34:12 -0700

    Original Code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Kenneth Fujimura
+//09-12-2022 @ 9:48pm
+//Mini Challenge # 3 - Asking Questions
+//Console program greets the user, asks the user two questions, stores those answers as strings, and then display the answers in a single sentence back to the user.
+//Peer Review by: see mini challenge #1
+
+Console.Clear();
+
+Console.WriteLine("Hello! I have some questions for you. Please indulge me.");
+
+bool playAgain = true;
+
+//play again while loop
+while (playAgain == true) {
+    //user inputs
+    Console.WriteLine("What is your name? Please enter on the keyboard and press enter when you are finished: ");
+    string userName = Console.ReadLine();
+
+    Console.WriteLine("Very good. Now, please tell me what time you woke up today? ");
+    string wakeUpTime = Console.ReadLine();
+
+    //program output
+    Console.WriteLine("So you're telling me that your name is \"" + userName + "\", and that you woke up at \"" + wakeUpTime + "\"? Cool beans!");
+
+    //play again prompt with input validation
+    Console.WriteLine("Would you like to answer my questions again? Y/N");
+    bool answerCheck = true;
+    while (answerCheck == true) {
+        string playAgainInput = Console.ReadLine();
+        if (playAgainInput == "y" || playAgainInput == "Y") {
+            answerCheck = false;
+            continue;
+        } else if (playAgainInput == "n" || playAgainInput == "N"){
+            playAgain = false;
+            answerCheck = false;
+        } else {
+            Console.WriteLine("I do not understand. Please answer with 'Y' for yes, or 'N' for no.");
+            answerCheck = true;
+        }
+    }
+}
+Console.WriteLine("Arigato!");
