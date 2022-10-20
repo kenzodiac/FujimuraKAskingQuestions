cfa963c3f5338d9a72ed19ba30c56cb712aecfe4
Kenzodiac <kennethfujimura@gmail.com>
2022-10-20 14:59:57 -0700

    Finish

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 //10-18-2022
 //GitHub Challenge # 3 - Asking Questions
 //Console program greets the user, asks the user two questions, stores those answers as strings, and then display the answers in a single sentence back to the user.
-//Peer Review by:
+//Peer Review by: Everything worked I liked how I was able to put a very specific time like AM or PM. The code looked clean and was able to understand it the only critique I have for it is that I am able to type in anything for the time I woke up I think it would of been good if there was validation for it so that they are only able to type a number for the time also was when I push enter it also leaves the answers blank, other then that everything else looked good.
 
 Console.Clear();
 
